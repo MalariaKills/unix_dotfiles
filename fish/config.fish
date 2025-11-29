5a8b2bfbf7cad411f90c37bf7b2d84229d95908a
Euchey <95317404+MalariaKills@users.noreply.github.com>
2025-11-29 16:29:32 -0600

    Clean up fish config and remove comments
    Removed unnecessary comments and adjusted formatting.

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,3 @@ set -g fish_greeting ""
 if status is-interactive; and test -t 1
     fastfetch
 end
-
-export SUDO_EDITOR="$(which nvim)"
-# or alternatively:
-#export VISUAL="$(which nvim)" EDITOR="$(which nvim)"
